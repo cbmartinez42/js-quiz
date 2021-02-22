dababa04c8480f5e9dd831090695c993a9d1b20b
Chris Martinez <cbmartinez42@gmail.com>
2021-02-21 18:11:43 -0600

    updating with core files, some images, html and css formatting

---
+++ assets/scripts/script.js
@@ -0,0 +1,25 @@
+// current score box
+
+
+
+// high score box
+
+
+
+// quiz pool of questions? or maybe do in a separate sheet?
+
+
+
+// keep running tally to refer to the current score box
+
+
+
+// save score server-side so that it can be added to high score box if necessary
+
+
+
+// timer
+
+
+// start/submit button for quiz. data-states will be active/inactive
+// inactive state will be start/active state will be quit or submit
